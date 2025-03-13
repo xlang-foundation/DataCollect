8976b1b5ae985e3aa2de154441d20a052267fb9c
gaoyi <gaoyia@qq.com>
2025-03-13 08:44:20 +0800

    切换路由模式

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,9 +1,10 @@
 // src/router/index.ts
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Layout from '@/layout/index.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/admin/login',
